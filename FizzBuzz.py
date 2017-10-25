67af60bae0fc6073cb3349ce79c835fcbd533ed3
Tokori <tomo_jmh@yahoo.co.jp>
2017-10-25 02:28:13 +0000

    fizzbuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,11 @@
-number = int(15)
-if (number % 3) == 0 and (number % 5) == 0:
-    print("Fizz Buzz")
-elif (number % 3) == 0:
-    print("Fizz")
-elif (number % 5) == 0:
-    print("Buzz")
-else:
-    print(str(number))
+
+for i in range(101):
+    number = int(i)
+    if (number % 3) == 0 and (number % 5) == 0:
+        print("Fizz Buzz")
+    elif (number % 3) == 0:
+        print("Fizz")
+    elif (number % 5) == 0:
+        print("Buzz")
+    else:
+        print(str(number))
